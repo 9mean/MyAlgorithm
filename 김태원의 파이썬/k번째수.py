d4a9c627471dfa6ba35c81bd45519a1dd859110b
9mean <kanggm102@naver.com>
2020-12-27 15:14:16 +0900

    20201227 python start

---
+++ 김태원의 파이썬/k번째수.py
@@ -0,0 +1,8 @@
+t=int(input())
+answer=[[0]for _ in range(2)]*t
+for i in range(t):
+  n,s,e,k=map(int,input().split())
+  a=list(map(int,input().split()))
+  a=a[s-1:e]
+  a.sort()
+  print("#%d %d" %(i+1,a[k-1]))
